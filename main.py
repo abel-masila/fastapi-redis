f89bbcf8498bc099ed426e56758bd7fa7333bc2b
Abel Masila <abel@x2ai.com>
2022-08-18 02:38:24 +0300

    Working on events

---
+++ main.py
@@ -0,0 +1,68 @@
+import json
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+
+import consumers
+
+app= FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+redis= get_redis_connection(
+    host="redis-14158.c85.us-east-1-2.ec2.cloud.redislabs.com",
+    port=14158,
+    password="dLf9vJXL3FbLBJYDLzVV7kp3Q2JOqzR6",
+    decode_responses=True
+)
+
+class Delivery(HashModel):
+    budget: int = 0
+    notes: str = ""
+
+    class Meta:
+        database = redis
+
+class Event(HashModel):
+    delivery_id: str = None
+    data: str
+    type: str
+
+    class Meta:
+        database = redis
+
+@app.get("/deliveries/{pk}/status")
+async  def get_state(pk: str):
+    state= redis.get(f'delivery:{pk}')
+    return json.loads(state)
+
+@app.post("/deliveries/create")
+async def create(request: Request):
+    body = await request.json()
+    delivery = Delivery(budget=body["data"]["budget"], notes=body["data"]["notes"]).save()
+    event = Event(delivery_id=delivery.pk, type=body["type"], data= json.dumps(body["data"])).save()
+    state= consumers.create_delivery({}, event)
+    redis.set(f'delivery:{delivery.pk}', json.dumps(state))
+    return state
+
+@app.post('/event')
+async def dispatch(request:Request):
+    body = await request.json()
+    delivery_id= body["delivery_id"]
+    event = Event(delivery_id=delivery_id, type=body["type"], data= json.dumps(body["data"])).save()
+    state=await get_state(delivery_id)
+    new_state= consumers.start_delivery(state, event)
+    redis.set(f'delivery:{delivery_id}', json.dumps(new_state))
+    return new_state
+
+
+
+
+
+
+
+
